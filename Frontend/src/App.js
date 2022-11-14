57513452546dc3def04379fd2ed1067c00f5bd1f
Dulya Perera <it19236810@my.sliit.lk>
2022-11-14 09:13:04 +0530

    changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
             </Switch>
           </BrowserRouter>
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     )
   }
