f92c719706f18b538cf95210b6125a45aafcdb9f
Methmi Nugawela <methmi.nuga@gmail.com>
2022-11-16 16:45:34 +0530

    Created access control middleware

--- Backend/src/api/user.api.js
+++ Backend/src/api/user.api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { verifyWorker } = require("../../middleware/verifyToken");
 const router = express.Router();
 const userController = require("../controller/user.controller");
 
@@ -8,6 +9,10 @@ module.exports = function () {
   // router.get('/', userController.getAll);
   router.get("/getrole/:email", userController.setUserRole);
   router.post("/update/:userId", userController.updateUser);
+
+  // TODO - Should implement message and file sending functionalitites
+  router.post("/user/post-message", verifyWorker, userController.postMessage);
+  router.post("/user/upload-file", verifyWorker, userController.uploadFile);
 
   return router;
 };
